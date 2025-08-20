cc5009a1f3b348b72a52a46bb3160dc5a5fd8617
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-08-20 13:54:58 +0400

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center min-h-screen">
+        Alpha Factory
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
