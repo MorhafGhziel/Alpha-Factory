371822e2435be0f8c1c293ce8b1e88838dcea4f4
aghyadghziel <aghyadghziel@gmail.com>
2025-09-29 19:10:29 +0400

    update

--- src/app/client/tracking-board/page.tsx
+++ src/app/client/tracking-board/page.tsx
@@ -1022,16 +1022,7 @@ export default function ClientTrackingBoardPage() {
                           ) : (
                             <>
                               <button
-                                onClick={async () => {
-                                  // Toggle verification status manually
-                                  const currentStatus =
-                                    project.verificationMode;
-                                  const newStatus =
-                                    currentStatus === "متميز"
-                                      ? "لا شيء"
-                                      : "متميز";
-                                  await updateRating(project.id, newStatus);
-                                }}
+                            
                                 className="relative group cursor-pointer active:scale-95 transition-transform duration-150"
                               >
                                 <div className="relative w-6 h-6">
